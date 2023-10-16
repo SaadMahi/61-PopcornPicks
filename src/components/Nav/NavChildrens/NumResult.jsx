f0e6a1ab678968a7f06dd77866f82324d2b7b02b
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-10-16 18:46:53 +0530

    📱 feat: project made mobile responsive

--- src/components/Nav/NavChildrens/NumResult.jsx
+++ src/components/Nav/NavChildrens/NumResult.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function NumResult({ movies }) {
   return (
-    <p className='num-results'>
+    <p className='num-results hideOnSm'>
       Found <strong>{movies.length}</strong> results
     </p>
   );
