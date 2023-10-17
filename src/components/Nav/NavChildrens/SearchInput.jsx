b3b9b263e3d339f85fc740ee1f571cc1e080bfc1
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-10-18 02:43:59 +0530

    ⚡️ feat: added Enter keydown to search input

--- src/components/Nav/NavChildrens/SearchInput.jsx
+++ src/components/Nav/NavChildrens/SearchInput.jsx
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 function SearchInput({ searchValue, setSearchValue }) {
+  const inputEl = useRef(null);
+
+  useEffect(() => {
+    console.log(inputEl.current);
+
+    // * reason to use this separate call back function is so we can clean up after our event
+    const searchInputFocus = function (e) {
+      // ! when your input element is active it will simply return instead of making your query empty
+      if (document.activeElement === inputEl.current) return;
+
+      if (e.code === 'Enter') {
+        inputEl.current.focus();
+        setSearchValue('');
+      }
+    };
+
+    document.addEventListener('keydown', searchInputFocus);
+
+    // ! clean up function
+    return () => {
+      document.removeEventListener('keydown', searchInputFocus);
+    };
+  }, [setSearchValue]);
+
   return (
     <input
       className='search'
@@ -8,6 +32,7 @@ function SearchInput({ searchValue, setSearchValue }) {
       placeholder='Search movies...'
       value={searchValue}
       onChange={(e) => setSearchValue(e.target.value)}
+      ref={inputEl}
     />
   );
 }
